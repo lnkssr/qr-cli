adf0b9901ccf00645ed595bb148786457dc8d988
lnkssr <gifzoner5@gmail.com>
2024-09-13 11:42:22 +0300

    add writing in file

--- src/main.go
+++ src/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"os"
 
 	"github.com/skip2/go-qrcode"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: qr-cli <text for qr-code>")
+	consoleFlag := flag.Bool("c", false, "Print QR code to console")
+	fileFlag := flag.String("f", "", "File path to save QR code image")
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: qr-cli [-c | -f <file>] <text for qr-code>")
 		return
 	}
 
-	input := os.Args[1]
+	input := flag.Args()[0]
 
 	qrCode, err := qrcode.New(input, qrcode.Medium)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	qrString := qrCode.ToString(false)
-
-	fmt.Println(qrString)
+	if *fileFlag != "" {
+		err = qrCode.WriteFile(256, *fileFlag)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("QR code saved to file: %s\n", *fileFlag)
+	} else if *consoleFlag {
+		qrString := qrCode.ToString(false)
+		fmt.Println(qrString)
+	} else {
+		fmt.Println("No valid flag provided. Use -c to print to console or -f <file> to save to a file.")
+	}
 }
